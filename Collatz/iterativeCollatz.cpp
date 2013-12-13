a3558ebebb4be921a123b0dbe73a88e0b24cad9a
karlCobb <malec038@umn.edu>
2013-12-13 13:59:26 -0500

    Avoids segmentation fault using uint64_t

--- Collatz/iterativeCollatz.cpp
+++ Collatz/iterativeCollatz.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <ctime>
 using namespace std;
+int ITERATIONS=1000000;
+typedef uint64_t num;
+
 
 int main()
 {
+time_t start;
+time_t end;
+start = time(NULL);
 vector<int> maxArray;
 vector<int> newArray;
-int max_size = 0;
-int n = 2;
-int collatz = n;
-while(n <= 100000){
+num max_size = 0;
+num n = 2;
+num collatz = n;
+while(n <= ITERATIONS){
+
+try{
 newArray.push_back(collatz);
+}catch(exception& e){
+cerr << e.what() << endl;
+cerr << "size: " << newArray.size() << "\n";
+cerr << "capacity: " << newArray.capacity() << "\n";
+}
+
 if(collatz == 1){
-n++;
+++n;
 if(newArray.size() > max_size){
 maxArray.clear();
-for(vector<int>::const_iterator i = newArray.begin(); i < newArray.end(); ++i){
+for(vector<int>::const_iterator i = newArray.begin(); i != newArray.end(); ++i){
 maxArray.push_back(*i);
 }
 max_size = newArray.size();
@@ -29,10 +44,11 @@ collatz = collatz/2;
 else
 collatz = 3*collatz+1;
 }
-for(vector<int>::const_iterator i = maxArray.begin(); i < maxArray.end(); ++i){
+for(vector<int>::const_iterator i = maxArray.begin(); i != maxArray.end(); ++i){
 cout << *i << " "; 
 }
-cout << "\n" << max_size;
+cout << "\n" << max_size << endl;
+end = time(NULL);
+cout << difftime(end, start)*1000 << "ms" << endl;
 }
-
 
