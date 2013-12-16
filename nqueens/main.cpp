1099331e5d7984880693e39ace23864d74c5d19e
karlCobb <malec038@umn.edu>
2013-12-16 17:15:20 -0500

    nqueens gives one solution

--- nqueens/main.cpp
+++ nqueens/main.cpp
@@ -23,21 +23,24 @@ if((diagonal(queens, N) && across(queens)) || (queens.size() == 1)){
 row = 1;
 }
 else{
-if(row < N){
+if(row <= N){
 Queen last_queen = queens.at(queens.size()-1);
 row = last_queen.getRow()+1;
+}
+while(row > N && queens.size() > 1){
 queens.pop_back();
-}
-else if(row == N){
-Queen last_queen = queens.at(0);
+Queen last_queen = queens.at(queens.size()-1);
 row = last_queen.getRow()+1;
-col = 1;
-queens.clear();
+col = last_queen.getColumn();
 }
+queens.pop_back();
+/*
 if(row == N+1){
 cout << "Impossible to solve with an " << N << "x" << N << " grid." << endl; 
 return 0;
 }
+*/
+
 }//end else there was a hit
 }//end while
 for(vector<Queen>::const_iterator it = queens.begin(); it != queens.end(); ++it)
